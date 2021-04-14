c8c5c0ab184536e9cd22b0881a08b2f450e18702
Thiago Lanza <lanzathm@gmail.com>
2021-04-13 21:40:25 -0300

    User resource

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use App\Models\Session;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'online' => false,
+            'session' => $this->sessionDetails($this->id)
+        ];
+    }
+
+    /**
+     * Get session details between 2 users.
+     *
+     * @param  int  $id
+     * @return array
+     */
+    private function sessionDetails(int $id)
+    {
+        return Session::whereIn('user1_id', [auth()->id(), $id])
+            ->whereIn('user2_id', [auth()->id(), $id])
+            ->first();
+    }
+}
