c7b44ecb853a936ba9dd00692aea29a649e019a0
Thiago Lanza <lanzathm@gmail.com>
2021-04-14 18:50:44 -0300

    Chat session broadcast event

--- routes/channels.php
+++ routes/channels.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Broadcast;
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+
+// Presence channel to return who's connected to this channel.
+Broadcast::channel('Chat', function ($user) {
+    return $user;
+});
